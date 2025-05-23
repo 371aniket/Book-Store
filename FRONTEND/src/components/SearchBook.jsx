52edbff33d2d88a84ae490dc8de29b458a6871a2
Aniket Kumar <160092558+371aniket@users.noreply.github.com>
2025-05-24 00:43:41 +0530

    Update SearchBook.jsx

--- FRONTEND/src/components/SearchBook.jsx
+++ FRONTEND/src/components/SearchBook.jsx
@@ -13,7 +13,7 @@ const SearchBook = () => {
     }
 
     try {
-      const res = await axios.get(`https://book-store-backend-smid.onrender.com?title=${query}`);
+      const res = await axios.get(`https://book-store-backend-smid.onrender.com/search?title=${query}`);
       setBooks(res.data);
       setSearched(true);
     } catch (error) {
